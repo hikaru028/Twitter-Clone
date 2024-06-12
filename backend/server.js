235d0a493da9c5f9af7b53ecea1058ef79150ce2
whereishikaru <h.suzuki.028@gmail.com>
2024-06-12 19:55:53 +1200

    Created DB

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 // Routes
-import authRoute from './routes/authRoute.js';
+import authRoute from './routes/auth.js';
+// Database
+import connectMongoDB from './db/connectMongoDB.js';
+
+dotenv.config();
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoute);
 
 // Run server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+    connectMongoDB();
 });
