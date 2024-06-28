f6eac3ad8b2db034678fb9e82528b1fded703e7f
whereishikaru <h.suzuki.028@gmail.com>
2024-06-28 22:14:24 +1200

    Feat: added .babelrc file

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,12 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!bson|other-module-to-transform)'
+  ],
   testTimeout: 30000,
   maxWorkers: 2,
   verbose: true,
